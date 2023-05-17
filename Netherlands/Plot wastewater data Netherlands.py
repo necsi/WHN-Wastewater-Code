83712021c3a399423ca22065d9f9946fa22abcb2
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2023-05-16 23:50:21 -0400

    Update Plot wastewater data Netherlands.py

--- Netherlands/Plot wastewater data Netherlands.py
+++ Netherlands/Plot wastewater data Netherlands.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-df = pd.read_json('https://data.rivm.nl/covid-19/COVID-19_rioolwaterdata.json')
+df = pd.read_csv('https://raw.githubusercontent.com/necsi/WHN-Wastewater-Data/main/data/Netherlands/nl_wastewater_data.csv')
 
 print(df.head())
 
